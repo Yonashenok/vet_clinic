e7059280b33cc244bff30a9858559719da64009b
Yonashenok <yonashenok24@gmail.com>
2023-08-22 14:21:31 +0300

    Create two more table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD species varchar(255);
 
+ALTER TABLE animals
+DROP COLUMN species; 
+
+ALTER TABLE animals 
+ADD species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals 
+ADD owner_id INTEGER REFERENCES owners(id);
+
+CREATE TABLE owners (
+    id  SERIAL PRIMARY KEY,
+    full_name varchar(100),
+    age int,
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100)   
+);
+
