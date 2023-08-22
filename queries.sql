a6486cba97e0bf2f3540fc516735379acc18aedc
Yonashenok <yonashenok24@gmail.com>
2023-08-22 09:05:04 +0300

    More new queries

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-
 SELECT * FROM animals WHERE name LIKE '%mon%'
 SELECT name FROM animals WHERE data_of_birth > '2016-01-01' AND data_of_birth < '2019-01-01'
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3
@@ -10,3 +9,50 @@ SELECT * FROM animals WHERE neutered = TRUE
 SELECT * FROM animals WHERE name != 'Gabumon'
 SELECT name FROM animals WHERE weight_kg >= 10.4  AND weight_kg <= 17.3 
 
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+    SET species = 'digimon'
+    WHERE name LIKE '%mon%';
+UPDATE animals
+    SET species = 'pokemon'
+   WHERE name NOT LIKE '%mon%';
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE data_of_birth > '2022-01-01';
+SAVEPOINT data_birth;
+UPDATE animals SET weight_kg = weight_kg * -1; 
+ROLLBACK To SAVEPOINT data_birth;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+COMMIT;
+SELECT * FROM animals;
+
+SELECT count(*) FROM animals
+
+SELECT count(*) FROM animals WHERE escape_attempts = 0
+
+SELECT avg(weight_kg) FROM animals 
+
+SELECT name, max(weight_kg) FROM animals 
+    GROUP BY name 
+    ORDER BY max DESC LIMIT 1;
+
+SELECT min(weight_kg), max(weight_kg) FROM animals 
+
+SELECT species, avg(escape_attempts) FROM animals 
+    WHERE data_of_birth > '1990-01-01' AND data_of_birth < '2000-01-01' 
+    GROUP by species
